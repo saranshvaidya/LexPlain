db0b90b7308a1000872f7a965bc550854019bdc1
saransh vaidya <saranshvaidya1795@gmail.com>
2026-02-28 17:21:44 +0530

    Fix pdf-parse types for Vercel build

---
+++ pdf-parse.d.ts
@@ -0,0 +1,13 @@
+declare module 'pdf-parse' {
+  interface PdfParseResult {
+    numpages: number
+    numrender: number
+    info: unknown
+    metadata: unknown
+    version: string
+    text: string
+  }
+
+  function pdfParse(dataBuffer: Buffer | Uint8Array): Promise<PdfParseResult>
+  export default pdfParse
+}
